91ef7355df8e950c5e203bf4e27a62c5e1e79dda
camily-tec <camily.barbosa2005@gmail.com>
2026-04-18 13:00:29 -0300

    menu inicial

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
-<?php
-echo "sistema de suporte rodando!";
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Suporte</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f4f6f8;
+            margin: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 100px auto;
+            text-align: center;
+            background: white;
+            padding: 40px;
+            border-radius: 10px;
+            box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+        }
+
+        h1 {
+            margin-bottom: 30px;
+        }
+
+        a {
+            display: block;
+            margin: 10px 0;
+            padding: 12px;
+            text-decoration: none;
+            background: #982aa8;
+            color: white;
+            border-radius: 5px;
+            transition: 0.2s;
+        }
+
+        a:hover {
+            background: #341539;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>Sistema de Suporte</h1>
+
+    <a href="novo_chamado.php"> Novo Chamado</a>
+    <a href="listar.php"> Listar Chamados</a>
+
+</div>
+
+</body>
+</html>
